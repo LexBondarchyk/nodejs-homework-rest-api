c788150ac1c7a61f024e1f94de1c5d0fd81b09b2
LexBondarchyk <108199972+LexBondarchyk@users.noreply.github.com>
2023-04-21 15:56:26 +0300

    post

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const {DB_HOST = 'mongodb+srv://Lex:Zktutylf21@cluster0.tblfitv.mongodb.net/db-contacts', PORT = 3000} = process.env;
+const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose.connect(DB_HOST)
   .then(() => {
